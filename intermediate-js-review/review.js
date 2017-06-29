c9e1b6494b1b3a4acdf6df3ad85c06d2dca19450
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-06-29 17:00:30 -0600

    done

--- intermediate-js-review/review.js
+++ intermediate-js-review/review.js
@@ -21,15 +21,15 @@ function theOverlord() {
 
 //Assign your answers to the variables below.
 //1. The value of rules if accessed from thePowerlessFigurehead
-var ans1 = '';
+var ans1 = 'love me plz';
 //2. The value of rules if accessed from theJudge
-var ans2 = '';
+var ans2 = 'cry all day';
 //3. The value of job if accessed from theOverlord
-var ans3 = '';
+var ans3 = undefined;
 //4. The value of job if accessed from thePowerlessFigurehead
-var ans4 = '';
+var ans4 = undefined;
 //5. The value of money if accessed from theTragicHumanitarian
-var ans5 = '';
+var ans5 = 'belongs to the overlord';
 
 ///Promises///
 
@@ -46,27 +46,37 @@ var fibonacci = function(n) {
     }
 };
 
-
-
+var iPromise = (num) => {
+  var defer = $q.defer()
+  defer.resolve(fibonacci(38))
+  return defer.promise;
+}
+iPromise().then((res) => {
+  console.log(res);
+})
 ///Context///
 
 //7. Inside contextIt, inkoke myFn with its context set explicitly
 /////to the contextObj, and with the two parameters passed in - return
 /////the result.
 function contextIt(myFn, contextObj, param1, param2) {
-
+return myFn.call(contextObj, param1, param2);
 }
 
 //8. Do the same as you did in question 6, except with the
 /////paramArray passed in instead
 function contextIt2(myFn, contextObj, paramArray) {
-
+  console.log(paramArray);
+var myfun = myFn.apply(contextObj, paramArray);
+return myfun
 }
 
 //9. Inside contextIt3, return a new function that permanently
 /////links myFn to the contextObj
 function contextIt3(myFn, contextObj) {
-
+  var myfun = myFn.bind(contextObj);
+  console.log(myfun());
+  return myfun
 }
 
 
@@ -77,13 +87,21 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 /////parameters called hornColor, magicType, mana, and
 /////canFly; inside the function, assign each to an identically
 /////named property
-
+function Unicorn(hornColor, magicType, mana, canFly) {
+  this.hornColor = hornColor;
+  this.magicType = magicType;
+  this.mana = mana;
+  this.canFly = canFly;
+}
 
 //question updated 03/30/17
 //11. Add a prototype method to Unicorn called castBubbleWrapSpell
 /////which adds 100 to the global variable bubbles and takes away
 /////15 from mana.
-
+Unicorn.prototype.castBubbleWrapSpell = function () {
+  bubbles += 100;
+  this.mana -= 15;
+};
 
 
 /////Closures/////
@@ -91,7 +109,11 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 //12. Write a function called greetingMaker which takes in a name
 /////and returns a function that takes in a greeting and returns
 /////the name and the greeting concatenated together (in that order)
-
+var greetingMaker = (name) => {
+  return (greeting) => {
+    return `${name}${greeting}`
+  }
+}
 
 //13. Write a function called countdownMaker which takes in an
 /////event name (string) and how many days until it happens.
@@ -100,10 +122,23 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 /////object that looks like this:
 /////{event: <eventname>, daysLeft: <days left>}
 
-
+var countdownMaker = (eName, days) => {
+  var eventName = eName;
+  var leftDays = days
+  return () => {
+    leftDays--
+    return {
+      event: eventName,
+      daysLeft: leftDays
+    }
+  }
+}
 
 /////Type Checking/////
 
 //14. Write a function called notMyType which takes in a parameter
 /////and returns the type of data of the parameter (number, string,
 /////boolean, etc)
+var notMyType = (parm) => {
+  return typeof parm
+}
